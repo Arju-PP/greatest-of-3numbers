704dbc6f75f8a636d3c105f3d39bced670ce3ad1
Arjun P P <75205434+Arju-PP@users.noreply.github.com>
2023-04-19 15:07:14 +0530

    Update Untitsled-1.py

--- Untitsled-1.py
+++ Untitsled-1.py
@@ -15,9 +15,9 @@ def greatest(a,b,c):
 
 
 st.subheader("Enter Numbers")
-a=st.number_input("Enter a")
-b=st.number_input("Enter b")
-c=st.number_input("Enter c")
+a=st.number_input("Enter a",step=1)
+b=st.number_input("Enter b",step=1)
+c=st.number_input("Enter c",step=1)
 
 
 st.write("The greatest Among the 3 Numbers is: ",greatest(a,b,c))
