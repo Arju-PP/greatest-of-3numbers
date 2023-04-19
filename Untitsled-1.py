d19f2ac9c387d26ef1b93c7541ac2f9ec5aa0b8e
Arjun P P <75205434+Arju-PP@users.noreply.github.com>
2023-04-19 14:14:33 +0530

    Update Untitsled-1.py

--- Untitsled-1.py
+++ Untitsled-1.py
@@ -1,10 +1,4 @@
 def greatest(a,b,c):
-    l=[]
-    l=[a,b,c]
-    l.sort()
-    print(l[-1])
-
-def g2(a,b,c):
     if a>b:
         if a>c:
             return a
@@ -15,8 +9,4 @@ def g2(a,b,c):
             return b
         else:
             return c
-        
-print(g2(1141,-1,100))
-            
-
-greatest(1,33,-1)
+ 
