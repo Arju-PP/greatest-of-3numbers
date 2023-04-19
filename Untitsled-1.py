0e9635e46c900d920dbd985e530828000c2016fa
Arjun P P <75205434+Arju-PP@users.noreply.github.com>
2023-04-19 14:50:27 +0530

    Update Untitsled-1.py

--- Untitsled-1.py
+++ Untitsled-1.py
@@ -1,3 +1,8 @@
+import streamlit as st
+
+st.write{"""
+Geatest Number selector among 3
+"""}
 def greatest(a,b,c):
     if a>b:
         if a>c:
@@ -9,8 +14,10 @@ def greatest(a,b,c):
             return b
         else:
             return c
- 
-a=input("Enter a")
-b=input("Enter b")
-c=input("Enter c")
+
+
+st.header("Enter Numbers")
+a=number_input("Enter a")
+b=number_input("Enter b")
+c=number_input("Enter c")
 print("Greatest of 3 is :",greatest(a,b,c))
